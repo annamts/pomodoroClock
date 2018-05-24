487d8e86a07f5cbdf80b55a906b285e9e45df063
unknown <annaterradas2@gmail.com>
2018-05-24 20:57:01 +0200

    Changed style, fixed bugs

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-/*********************************************\
+/******************************************************************************************\
 TO DO:
-    - stop button doesn't always work
-    - pause button is a bit buggy
-    - what if you stop while timer is paused?
+    - add mute button
+    - Sure you want to stop?
+
+    - fix options div
+    - add the sessions
+    - change pop-up div
+    - make apply button prettier
+
+    - figure out how to size everything relative to screen height
     
-\*********************************************/
+\******************************************************************************************/
 
 var minutes;
 var seconds;
 var work = true;
 var int;
 var total = 0;
-var going = false;
+var going = false; //to avoid starting, stopping or pausing when timer is running
+var paused = false; //to avoid starting when timer is paused
+var audio = new Audio('huawei-ripple.mp3');
+
 
 function start(x) {
-    if (!going) {
+    if (!going && !paused) {
         var left = x*60;
         minutes = addZero(Math.floor(left/60));
         seconds = addZero(left % 60);
@@ -40,6 +49,7 @@ function start(x) {
 }
 
 function addZero (x) {
+    //Adds a zero to the left if x is a single digit
     if (String(x).length < 2) {
         return "0" + x;
     } else {
@@ -48,6 +58,7 @@ function addZero (x) {
 }
 
 function info() {
+    audio.play();
     if (work) {
         total += 1
         $("#total").html(total);
@@ -66,6 +77,7 @@ function info() {
         );         
     }
     $("#ok").on("click", function () {
+        audio.pause();
         $("#info").addClass("popOut");
         if (work) {
             start($("#break").val());
@@ -82,21 +94,25 @@ function removeInfo() {
     $("#info").remove();
 }
 
+
 function pauseTimer () {
-    clearInterval(int);
-    $("#time").html(minutes + ":" + seconds);
-    $("body").prepend(
-        '<div id="info" class="popIn">' +
-            'Pomodoro is paused'+
-            '<button id="keepGoing">Keep going!</button>'+
-        '</div>'  
-    );
-    $("#keepGoing").on("click", function () {
-        $("#info").addClass("popOut");
+    if (going) {
         going = false;
-        start(decimalMinutes($("#time").html()));
-        setTimeout(removeInfo, 300);
-    });
+        paused = true;
+        clearInterval(int);
+        $("#time").html(minutes + ":" + seconds);
+        $("#pauseDiv").html(
+            '<button class="options" onclick="resume()"><i class="fa fa-play"></i></button>'
+        );
+    }
+}
+
+function resume () {
+    paused = false;
+    start(decimalMinutes($("#time").html()));
+    $("#pauseDiv").html(
+        '<button class="options" onclick="pauseTimer()"><i class="fa fa-pause"></i></button>'
+    );
 }
 
 function decimalMinutes (x) {
@@ -104,10 +120,15 @@ function decimalMinutes (x) {
 }
 
 function stop () {
-    clearInterval(int);
-    going = false;
-    console.log(int);
-    $("#time").html("00:00");
-    total = 0;
-    $("#total").html(total);
+    if (going) {
+        clearInterval(int);
+        going = false;
+        total = 0;
+        $("#total").html(total);
+        var work = $("#work").val()*60
+        minutes = addZero(Math.floor(work/60));
+        seconds = addZero(work % 60);
+        $("#time").html(minutes + ":" + seconds);  
+    }
 }
+
